ebb2bba3ad29e8cb570db67818d24cde638e2479
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-06 20:12:49 +0000

    datatypes

---
+++ 01Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict " ; //treat JS code as a newer version
+
+// alert(3+3) -> shows error -> because it runs in browser not on node 
+
+console.log(3 +
+    3
+    )  // not a good pratice readability reduces 
+
+    console.log("KD")
+
+    let name = "Kuldeep"
+    let age = 24 ;
+    let isloggedIn = false ;
+    let state ;
+
+    //  primitive datatypes 
+    /*
+number -> to assign numeric values
+bigInt -> higher numeric value
+null -> also a stand alone value (representation of empty value )
+undefined -> value not assigned but variable declared
+string -> "kuldeep"
+boolean -> true/false
+symbols -> uniqueness
+
+    */
+
+//  refernece datatypes 
+/*
+objects 
+arrays
+*/
