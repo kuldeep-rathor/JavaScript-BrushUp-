3c529d6acd206939df2ff7805aab33af9ce07ea7
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-08 11:43:22 +0000

    Conversion of dataTypes

---
+++ 01Basics/03_datatypeConversion.js
@@ -0,0 +1,36 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"12abc" => NaN
+null=> 0
+undefined => NaN
+"kuldeep"=> NaN 
+true => 1 , false => 0;
+
+
+
+
+*/
+
+let isLoggedIn = -12;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
+
+/* 
+ 1 => true , 0 => false 
+ ""=> false 
+ "kuldeep"=> true
+*/
+
+let someNumber = 12 ;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(someNumber);
+
