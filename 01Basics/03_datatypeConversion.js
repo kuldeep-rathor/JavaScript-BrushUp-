55b373039ad9e8d76d3c99a3cb11d78536ec7137
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-08 12:13:12 +0000

    operations

--- 01Basics/03_datatypeConversion.js
+++ 01Basics/03_datatypeConversion.js
@@ -21,7 +21,7 @@ true => 1 , false => 0;
 
 let isLoggedIn = -12;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn); 
+// console.log(booleanIsLoggedIn); 
 
 /* 
  1 => true , 0 => false 
@@ -31,6 +31,54 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 12 ;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(someNumber);
+// console.log(typeof stringNumber);
+// console.log(someNumber);
 
+//    operations 
+
+let value = 45 ;
+let negValue = -value;
+// console.log(negValue);
+
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/6);
+console.log(2%20);
+console.log(2**2);
+*/
+
+let str1 = "Hello";
+let str2 = " Kuldeep";
+let str3 = str1 + str2 ;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)  ;
+// console.log(1 + 2 + "2");
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+/*
+let num1 , num2 , mum3
+num1=num2=num3 = 2 + 2 => Bad practice of writing a code 
+
+*/
+// let gameCounter = 100;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+// gameCounter++;
+// console.log(gameCounter);
+
+let myLife = 23;
+// let Hp = ++myLife;
+// console.log(`${myLife}  ${Hp}`);
+let hP = myLife++;
+console.log(`${myLife} ${hP}`);
+console.log(hP)
