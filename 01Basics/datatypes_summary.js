ae29195b6f3917c841afda08886c6761c5d54ebf
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-09 17:14:51 +0000

    Stack and Heap

--- 01Basics/datatypes_summary.js
+++ 01Basics/datatypes_summary.js
@@ -1,5 +1,5 @@
 // primitive datatypes 
-console.log("Kuldeep")
+// console.log("Kuldeep")
  /*
 primitive -> string, number , null , boolean , undefined , BigInt , symbol
 
@@ -8,10 +8,10 @@ primitive -> string, number , null , boolean , undefined , BigInt , symbol
  */
 let id = Symbol('kd');
 let id2 = Symbol('kd');
-console.log(id === id2);
+// console.log(id === id2);
 
 let bigNumber =  456789123467n;
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 /*
 reference or non- primitive -> Arrays , Objects , functions 
 */
@@ -26,7 +26,25 @@ let  myObject = {
 const myFunction = function() {
     console.log('hello babe')
 }
-myFunction();
-console.log(typeof myFunction)
-console.log(typeof heros);
-console.log(typeof myObject);
+// myFunction();
+// console.log(typeof myFunction)
+// console.log(typeof heros);
+// console.log(typeof myObject);
+
+
+let myName = "kd";
+let newName = myName;
+newName= "kuldeep";
+// console.log(myName);
+// console.log(newName);
+
+let myObj = {
+    name : "kuldeep",
+    age : 22
+}
+
+let obj2 = myObj ;
+myObj.name = "kd";
+
+console.log(myObj.name);
+console.log(obj2.name);
