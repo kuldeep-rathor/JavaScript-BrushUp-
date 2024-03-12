491b7f0bc75f689dca49efd3b863f6910cce527c
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-12 07:53:31 +0000

    JSON - Objects

---
+++ 02Basics/05_objects.js
@@ -0,0 +1,22 @@
+const course = {
+    courseName  : "Basics of JAvascript",
+    price : "999",
+    courseInstructor : " Nil"
+}
+// course.courseInstructor
+
+const  {courseInstructor : Instructor}= course;
+console.log(Instructor);
+
+
+// {
+//     "name" : "Kuldeep",
+//     "courseName" : "Js basics",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
