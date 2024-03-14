80949f24c280420094c90a2852acfaa8ba75fa68
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-14 06:02:24 +0000

    Scope and mini Hoisgting in js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -13,3 +13,43 @@ var c =30;
 console.log(a);
 // console.log(b);
 console.log(c);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function one(){
+    const userName = "Kuldeep";
+    function two(){
+        const website = " Youtube.com";
+        console.log(userName)
+    }
+    // console.log(website);
+    two();
+}
+one();
+
+
+if (true) {
+    const user = "Rathor";
+    if(user === "Rathor"){
+        const web = " Youtube";
+        console.log(user + web);
+    }
+    // console.log(web);
+    
+}
+// console.log(user);
+
+// +++++++++++++++++++++++Interesting +++++++++++++
+console.log(addOne(5)); // no error here
+function addOne(num){
+    return num+1;
+}
+// addOne();
+
+addTwo();   // This error is occured due to hoisting concept due 
+const addTwo =function (num){
+    return num+2;
+}
+// addTwo();
