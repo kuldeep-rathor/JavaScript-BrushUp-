f0ad4343d0b65f911f7ef616643cd6f0e5221ded
Kuldeep Rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2024-03-14 06:38:52 +0000

    IIFE in JS

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log("DB CONNECTED");
+})();
+
+
+(()=>{
+    console.log("DB CONNECTED 2")
+}) ();
+
+((name)=>{
+    console.log(`DB CONNECTED 2 ${name}`)
+}) ("KD");
